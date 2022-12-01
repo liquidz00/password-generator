3b7be4001fbff84978350f2a43c935a03f758531
wh0aswerve <114184256+aspeciale@users.noreply.github.com>
2022-12-01 15:15:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,11 @@ special_chars = string.punctuation
 
 alphabet = letters + digits + special_chars
 
-pwd_length = int(input("How many characters long? Choose 12 or 16"))
+pwd_length = int(input("How many characters long? Choose 12 or 16: "))
+match pwd_length:
+    case 12:
+        print("Your number is 12")
+    case 16:
+        print("Your number is 16")
+    case other:
+        print("Please enter either 12 or 16")
