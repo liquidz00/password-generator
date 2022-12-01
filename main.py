9527b17a5cd15639447af99a150c43ecccc9c1c7
wh0aswerve <114184256+aspeciale@users.noreply.github.com>
2022-12-01 15:35:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import secrets
 import string
 
-letters = string.ascii_letters
-digits = string.digits
-special_chars = string.punctuation
+loop = 0
 
-alphabet = letters + digits + special_chars
+while loop == 0:
+    letters = string.ascii_letters
+    digits = string.digits
+    special_chars = string.punctuation
 
-pwd_length = int(input("How many characters long? Choose 12 or 16: "))
-match pwd_length:
-    case 12:
-        print("Your number is 12")
-    case 16:
-        print("Your number is 16")
-    case other:
-        print("Please enter either 12 or 16")
+    alphabet = letters + digits + special_chars
+
+    pwd_length = int(input("How many characters long? Choose 12 or 16: "))
+    match pwd_length:
+        case 12:
+            pwd_length = 12
+        case 16:
+            pwd_length = 16
+        case other:
+            print("Please enter either 12 or 16")
+
+    pwd = ""
+    for char in range(pwd_length):
+        pwd += "".join(secrets.choice(alphabet))
+
+    print(pwd)
+    print("Would you like to generate another password? [Y/N]")
+    response = str(input()).lower()
+    match response:
+        case "y":
+            loop = 0
+        case "n":
+            loop = 1
+
+    # if (
+    #     any(char in special_chars for char in pwd)
+    #     and sum(char in digits for char in pwd) >= 2
+    # ):
+    #     break
