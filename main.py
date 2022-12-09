a7b0654c818b50a209ab7e70153bc42266707e9e
wh0aswerve <114184256+aspeciale@users.noreply.github.com>
2022-12-09 14:33:09 -0500

    added generate function, updated main

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
 # Import necessary frameworks
-import secrets
-import string
 from shuffler import shuffle
+from generator import generate
 
 # Create main function
 def main():
-    # Define variables
-    uppercase_letters = string.ascii_letters.upper()
-    lowercase_letters = string.ascii_letters.lower()
-    digits = string.digits
-    special_chars = string.punctuation
-    alphabet = uppercase_letters + lowercase_letters + digits + special_chars
-
-    # Define variable length based upon user input
-    pwd_length = int(input("How many characters long? Choose 12 or 16: "))
-
-    # Assign value based upon user's response
-    match pwd_length:
-        case 12:
-            pwd_length = 12
-        case 16:
-            pwd_length = 16
-        case other:
-            print("Please enter either 12 or 16")
-
-    # Define empty string
-    pwd = ""
-
-    # Create for loop to add random choices from alphabet variable to password
-    for char in range(pwd_length):
-        pwd += "".join(secrets.choice(alphabet))
+    # Create a password using generate function
+    pwd = generate()
 
     # Shuffle output to verify password does not fit a unique pattern
     shuffle(pwd)
