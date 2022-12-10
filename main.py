2e350903638714d41954c9fd1611cc55698fd10b
aspeciale <114184256+aspeciale@users.noreply.github.com>
2022-12-10 15:43:28 -0500

    refactored code, updated main

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 # Import necessary frameworks
-from shuffler import shuffle
-from generator import generate
-from gui import window, clear_input
+import pwd_functions as pwdf
+import PySimpleGUI as sg
 
 
-# Create main function
-def main():
-    # Create a password using generate function
-    pwd = generate()
+# Show user window
+sg.theme("Reddit")
+titlebar = sg.Titlebar("Password Generator")
 
-    # Shuffle output to verify password does not fit a unique pattern
-    shuffle(pwd)
+layout = [
+    [sg.Text("How many characters long? Choose 12 or 16")],
+    [sg.Checkbox("12", default=False, enable_events=True, key="12 pwd")],
+    [sg.Checkbox("16", default=False, enable_events=True, key="16 pwd")],
+    [sg.InputText(key="pwd")],
+    [
+        sg.Button("Generate", enable_events=True, key="Generate"),
+        sg.Button("Clear"),
+        sg.Exit(),
+    ],
+]
 
-    # Show user generated password
-    print(pwd)
+window = sg.Window(titlebar, layout)
+
+# # Create a password using generate function
+# pwd = generate()
+
+# # Shuffle output to verify password does not fit a unique pattern
+# shuffle(pwd)
+
+# # Show user generated password
+# print(pwd)
+
+
+def clear_input():
+    for key in values:
+        window[key]("")
 
 
 while True:
-    main()
-    event, values = window.window.read()
-    if event == window.sg.WIN_CLOSED or event == "Exit":
-        window.window.close()
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == "Exit":
+        window.close()
         break
     if event == "Clear":
         print("information cleared")
-        window.window.clear_input()
+        pwdf.clear_input()
     if event == "Generate":
-        generate()
-        shuffle()
-
-    # response = input("Generate another password? (Y/N) ").strip().lower()
-    # match response:
-    #     case "y":
-    #         True
-    #     case "n":
-    #         print("Goodbye")
-    #         break
-    #     case other:
-    #         print("Please enter either Y or N")
+        print("Generate clicked")
+        # generate()
+        # shuffle()
---
+++ pwd_functions.py
@@ -0,0 +1,46 @@
+import string
+import secrets
+import random
+import main
+
+
+def shuffle(pwd=None):
+    list_shuffled = list(pwd)
+    random.shuffle(list_shuffled)
+    pwd = "".join(list_shuffled)
+    return pwd
+
+
+def generate():
+    # Define variables
+    uppercase_letters = string.ascii_letters.upper()
+    lowercase_letters = string.ascii_letters.lower()
+    digits = string.digits
+    special_chars = string.punctuation
+    alphabet = uppercase_letters + lowercase_letters + digits + special_chars
+
+    # Define variable length based upon user input
+    pwd_length = int(input("How many characters long? Choose 12 or 16: "))
+
+    # Assign value based upon user's response
+    match pwd_length:
+        case 12:
+            pwd_length = 12
+        case 16:
+            pwd_length = 16
+        case other:
+            print("Please enter either 12 or 16")
+
+    # Define empty string
+    pwd = ""
+
+    # Create for loop to add random choices from alphabet variable to password
+    for char in range(pwd_length):
+        pwd += "".join(secrets.choice(alphabet))
+
+    return pwd
+
+
+def clear_input():
+    for key in main.layout.values:
+        main.window[key]("")
