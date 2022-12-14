9ea0ca70802f79ada062b85b8493e6e4477cde3c
aspeciale <114184256+aspeciale@users.noreply.github.com>
2022-12-14 16:16:56 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+APP = ["main.py"]
+DATA_FILES = []
+OPTIONS = {
+    "argv_emulation": True,
+    "packages": ["tkinter", "random", "pyperclip"],
+    "iconfile": "app-icon.icns",
+    "plist": {
+        "CFBundleDevelopmentRegion": "English",
+        "CFBundleIdentifier": "com.speciale.xxx",
+        "CFBundleVersion": "1.0.0",
+    },
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
+)
