40f8fc3022e2c61147efebb891ac21a84cccb6a7
wh0aswerve <114184256+aspeciale@users.noreply.github.com>
2022-12-15 14:23:43 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ APP = ["main.py"]
 DATA_FILES = ["app-icon.icns", "copy-30.png"]
 OPTIONS = {
     "argv_emulation": True,
+    "iconfile": "app-icon.icns",
 }
 
 setup(
     app=APP,
+    name="Password Generator",
     data_files=DATA_FILES,
     options={"py2app": OPTIONS},
     setup_requires=["py2app"],
