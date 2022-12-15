84a05e903c48215f531866e6f64cfc30b7d56078
wh0aswerve <114184256+aspeciale@users.noreply.github.com>
2022-12-15 15:06:28 -0500

    updated setup and main

--- setup.py
+++ setup.py
@@ -5,7 +5,16 @@ DATA_FILES = ["app-icon.icns", "copy-30.png"]
 OPTIONS = {
     "argv_emulation": True,
     "iconfile": "app-icon.icns",
-    "packages": ["tkinter", "pyperclip"],
+    "packages": ["tkinter", "pyperclip", "string", "secrets", "random", "os"],
+    "includes": ["tkinter", "pyperclip", "string", "secrets", "random", "os"],
+    "plist": {
+        "CFBundleName": "Password Generator",
+        "CFBundleDisplayName": "Password Generator",
+        "CFBundleGetInfoString": "Generate Passwords",
+        "CFBundleIdentifier": "com.c0bras.passwordgenerator",
+        "CFBundleVersion": "1.0.0",
+        "CFBundleShortVersionString": "1.0.0",
+    },
 }
 
 setup(
